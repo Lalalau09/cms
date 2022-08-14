e9fa0616b9e3a55efbd09d50888a7e4b6b674de3
Lau Bissoni <Lalalau09@github.com>
2022-08-13 21:35:28 -0300

    otros commits

---
+++ index.js
@@ -0,0 +1,40 @@
+function addWorkCard(params = {}){
+    const template= document.querySelector("#portfolio-card-template")
+    const container= document.querySelector(".portfolio-content")
+    template.content.querySelector(".portfolio-card-title").textContent = params.title
+    template.content.querySelector(".portfolio-card-text").textContent = params.text
+    template.content.querySelector(".portfolio-card-link").href = params.url
+    template.content.querySelector(".portfolio-img").src = params.img
+
+    const clone= document.importNode (template.content, true)
+    container.appendChild(clone)
+}
+
+function getWorks(){
+
+    return fetch("https://cdn.contentful.com/spaces/2bj5h72pbrvp/environments/master/content_types/work?access_token=-HGj2bVPZ5nDelVnaV9aG9N987rhaMTkYeL1pkyKDxw"
+    ).then((res)=>{
+        return res.json()}
+        ).then((data)=>{
+    const fieldCollection = data.items.map((item)=>{
+        return{
+            title:item.fields.titulo}
+
+    })
+    return fieldCollection
+        }
+        )
+
+}
+function main(){
+getWorks().then(function(works){
+    console.log(works)
+    for (const w of works ){
+        addWorkCard(w)
+    }
+})
+
+
+}
+
+main()
